6993dcf40cf889d8e75a19c21a8fca233165533d
Arun Nagarathinam <a1nagar@L-SNV36PRG8W-M.local>
2015-09-11 10:42:45 -0700

    Testing Jenkins

--- src/test/java/Hema/Java/SimpleTest.java
+++ src/test/java/Hema/Java/SimpleTest.java
@@ -4,6 +4,7 @@ public abstract class SimpleTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Testing Jenkins automatic build");
 
 	}
 
