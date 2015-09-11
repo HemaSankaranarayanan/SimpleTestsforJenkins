ffe9f256a8846a7a632a3dbee7ff13e1e48c4971
Arun Nagarathinam <a1nagar@L-SNV36PRG8W-M.local>
2015-09-11 11:01:53 -0700

    Last test

--- src/test/java/Hema/Java/AnotherTest.java
+++ src/test/java/Hema/Java/AnotherTest.java
@@ -3,7 +3,7 @@ package Hema.Java;
 public abstract class AnotherTest {
 	
 	public static void main(String[] args) {
-		System.out.println("Testing Jenkins auto");
+		System.out.println("Testing Jenkins auto build");
 
 	}
 	
