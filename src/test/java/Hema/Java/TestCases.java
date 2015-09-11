9a552e63358ea53a0e667cd473c4e17175e7e898
Arun Nagarathinam <a1nagar@L-SNV36PRG8W-M.local>
2015-09-11 11:34:05 -0700

    Jacoco

---
+++ src/test/java/Hema/Java/TestCases.java
@@ -0,0 +1,24 @@
+package Hema.Java;
+
+import org.junit.Test;
+
+public class TestCases {
+	
+	@Test
+	public void TestCase1() {
+		
+	String a = "Hema";
+	String b = "Hema";
+	
+	if (a == b) {
+		System.out.println("Both are equal");
+		
+	}
+	else {
+		System.out.println("Both are not equal");
+	}
+		
+		
+	}
+
+}
